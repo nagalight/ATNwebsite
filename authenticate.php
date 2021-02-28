068d856f28027b4da82b942c0283fb3a90c3f900
nagalight <57606060+nagalight@users.noreply.github.com>
2021-02-28 21:52:07 +0700

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -16,14 +16,14 @@ if (!$pg_heroku)
     exit('Error: Could not connect: ' . pg_last_error());
   }
 
-if ( !isset($_POST['username'], $_POST['password']) ) {
+if ( !isset($_POST[username], $_POST[password]) ) {
 	// Could not get the data that should have been sent.
 	exit('Please fill both the username and password fields!');
 }
 
-if ($stmt = $pg_heroku->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+if ($stmt = $pg_heroku->prepare(SELECT id, password FROM accounts WHERE username = ?)) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-	$stmt->bind_param('s', $_POST['username']);
+	$stmt->bind_param('s', $_POST[username]);
 	$stmt->execute();
 	// Store the result so we can check if the account exists in the database.
 	$stmt->store_result();
@@ -33,14 +33,14 @@ if ($stmt = $pg_heroku->prepare('SELECT id, password FROM accounts WHERE usernam
 	$stmt->fetch();
 	// Account exists, now we verify the password.
 	// Note: remember to use password_hash in your registration file to store the hashed passwords.
-	if (password_verify($_POST['password'], $password)) {
+	if (password_verify($_POST[password], $password)) {
 		// Verification success! User has logged-in!
 		// Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
 		session_regenerate_id();
-		$_SESSION['loggedin'] = TRUE;
-		$_SESSION['name'] = $_POST['username'];
-		$_SESSION['id'] = $id;
-		echo 'Welcome ' . $_SESSION['name'] . '!';
+		$_SESSION[loggedin] = TRUE;
+		$_SESSION[name] = $_POST[username];
+		$_SESSION[id] = $id;
+		echo 'Welcome ' . $_SESSION[name] . '!';
 	} else {
 		// Incorrect password
 		echo 'Incorrect username and/or password!';
