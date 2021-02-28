de47318feb111eb56a04b98a59f0f0d5fff8fcbe
nagalight <57606060+nagalight@users.noreply.github.com>
2021-02-28 22:12:33 +0700

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -16,41 +16,19 @@ if (!$pg_heroku)
     exit('Error: Could not connect: ' . pg_last_error());
   }
 
-if ( !isset($_POST[username], $_POST[password]) ) {
-	// Could not get the data that should have been sent.
-	exit('Please fill both the username and password fields!');
+if(isset($_POST['Login'])&&!empty($_POST['Login'])){
+    
+    $hashpassword = md5($_POST['password']);
+    $sql ="select *from  where email = '".pg_escape_string($_POST['username'])."' and password ='".$hashpassword."'";
+    $data = pg_query($dbconn,$sql); 
+    $login_check = pg_num_rows($data);
+    if($login_check > 0){ 
+        
+        echo "Login Successfully";    
+    }else{
+        
+        echo "Invalid Details";
+    }
 }
-
-if ($stmt = $pg_heroku->prepare(SELECT id, password FROM accounts WHERE username = ?)) {
-	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-	$stmt->bind_param('s', $_POST[username]);
-	$stmt->execute();
-	// Store the result so we can check if the account exists in the database.
-	$stmt->store_result();
-  
-  if ($stmt->num_rows > 0) {
-	$stmt->bind_result($id, $password);
-	$stmt->fetch();
-	// Account exists, now we verify the password.
-	// Note: remember to use password_hash in your registration file to store the hashed passwords.
-	if (password_verify($_POST[password], $password)) {
-		// Verification success! User has logged-in!
-		// Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
-		session_regenerate_id();
-		$_SESSION[loggedin] = TRUE;
-		$_SESSION[name] = $_POST[username];
-		$_SESSION[id] = $id;
-		echo 'Welcome ' . $_SESSION[name] . '!';
-	} else {
-		// Incorrect password
-		echo 'Incorrect username and/or password!';
-	}
-} else {
-	// Incorrect username
-	echo 'Incorrect username and/or password!';
-}
-
-
-	$stmt->close();
 }
 ?>
