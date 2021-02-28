b68dea8fc1d352c097ec8812e6552692f82c2386
nagalight <57606060+nagalight@users.noreply.github.com>
2021-02-28 22:23:53 +0700

    Update home.php

--- home.php
+++ home.php
@@ -12,7 +12,6 @@ if (!isset($_SESSION['loggedin'])) {
 	<head>
 		<meta charset="utf-8">
 		<title>Home Page</title>
-		<link href="style.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	</head>
 	<body class="loggedin">
