712f620f3d310002270e81adc3bd8db36e46933b
nagalight <57606060+nagalight@users.noreply.github.com>
2021-02-28 22:53:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+$host_heroku = "ec2-34-203-255-149.compute-1.amazonaws.com";
+$db_heroku = "d642kjd2c1kho9";
+$user_heroku = "nkkmoipxidjucr";
+$pw_heroku = "d7e2d81d7683c4e0b617b5004f9f0c56f2accc2c28e3fcd58e5abdc8165aabcc";
+
+$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+$pg_heroku = pg_connect($conn_string);
+
+if(isset($_POST['submit'])&&!empty($_POST['submit'])){
+    
+    $hashpassword = md5($_POST['password']);
+    $sql ="select *from accounts where username = '".pg_escape_string($_POST['username'])."' and password ='".$hashpassword."'";
+    $data = pg_query($pg_heroku,$sql); 
+    $login_check = pg_num_rows($data);
+    if($login_check > 0){ 
+        
+        echo "Login Successfully";    
+    }else{
+        
+        echo "Invalid Details";
+    }
+}
+
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -18,7 +42,7 @@
 					<i class="fas fa-lock"></i>
 				</label>
 				<input type="password" name="password" placeholder="Password" id="password" required>
-				<input type="submit" value="Login">
+				<input type="submit" name="submit" value="Login">
 			</form>
 		</div>
 	</body>
