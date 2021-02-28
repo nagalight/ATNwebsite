03e775620a625252fde14bbc0b7fd2a6df603267
nagalight <57606060+nagalight@users.noreply.github.com>
2021-02-28 23:19:04 +0700

    Update home.php

--- home.php
+++ home.php
@@ -7,6 +7,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Home Page</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	</head>
 	<body class="loggedin">
