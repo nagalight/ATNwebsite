04e63b6d687768e7c543071b253fdbd47b6e0d8c
nagalight <57606060+nagalight@users.noreply.github.com>
2021-03-01 21:38:41 +0700

    Update home.php

--- home.php
+++ home.php
@@ -80,7 +80,7 @@ if (!$pg_heroku)
 	</div>
 	
 	<?php
-	if(isset($_POST['add']))
+	if(isset($_GET['add']))
 	{
 		$sql1 = "INSERT INTO products(name, value, in_store)  VALUES('$_GET['name']', $_GET['value'], $_GET['stock'])";
 		$result = pg_query($pg_heroku, $sql1);
