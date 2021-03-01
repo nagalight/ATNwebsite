74ad1c5e64d7c2dbdb148cda86f7a981e0ddb453
nagalight <57606060+nagalight@users.noreply.github.com>
2021-03-01 14:16:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,16 @@ $pw_heroku = "d7e2d81d7683c4e0b617b5004f9f0c56f2accc2c28e3fcd58e5abdc8165aabcc";
 $conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
 $pg_heroku = pg_connect($conn_string);
 
+if (!$pg_heroku)
+  {
+    die('Error: Could not connect: ' . pg_last_error());
+  }
+
 if(isset($_POST['login'])&&!empty($_POST['login'])){
     
     $hashpassword = md5($_POST['password']);
-    $sql ="select * from accounts where username = '".pg_escape_string($_POST['username'])."' and password ='".$hashpassword."'";
+    $name = pg_escape_string($_POST['username']);
+    $sql ="select * from accounts where username = '".$name."' and password ='".$hashpassword."'";
     $data = pg_query($pg_heroku,$sql); 
     $login_check = pg_num_rows($data);
     if($login_check > 0){ 
