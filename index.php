a3c07fa857ead1523739b97e712a3fc453b8f2b2
nagalight <57606060+nagalight@users.noreply.github.com>
2021-03-01 23:11:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,14 +12,16 @@ if (!$pg_heroku)
     exit('Error: Could not connect: ' . pg_last_error());
   }
 
-if(isset($_POST['login'])&&!empty($_POST['login'])){
+if(isset($_GET['login'])&&!empty($_GET['login'])){
     
     #$hashpassword = md5($_POST[password]);
-    $sql ="select * from accounts where username = '$_POST[username]' and password ='$_POST[password]'";
+    $sql ="select * from accounts where username = '$_GET[username]' and password ='$_GET[password]'";
     $data = pg_query($pg_heroku,$sql); 
     $login_check = pg_num_rows($data);
-    if($login_check > 0){ 
-        header('Location: home.php');    
+    if($login_check > 0)
+    { 
+        
+	header('Location: home.php');    
     }else{
         echo "Invalid Details";
         
